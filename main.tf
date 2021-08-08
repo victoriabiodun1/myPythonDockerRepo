bf0c42621bbc524a042748ae1d2910afbc5464a2
victoriabiodun1 <86543474+victoriabiodun1@users.noreply.github.com>
2021-08-07 21:55:01 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <style type="text/css">
+      body {
+        background: black;
+        color: green;
+      }
+      div.container {
+        max-width: 500px;
+        margin: 100px auto;
+        border: 20px solid white;
+        padding: 10px;
+        text-align: center;
+      }
+      h4 {
+        text-transform: uppercase;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h4>Welcome to PythonApp running on Docker containers inside Jenkins!!</h4>
+      <h4>We have automated Docker image creation & spinning up Containers using Jenkins!!</h4>
+      <h4>Automating using Jenkins, Docker, GitHub and Pipeline!!!!!</h4>
+      <br>
+       <h4>Anything is Possible!!!!!</h4>
+    </div>
+  </body>
+</html>
